9332b65782166e43653e3ed4426087f003e9adc0
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2022-09-30 17:55:20 +0200

    fix test build

--- tests/monitor.rs
+++ tests/monitor.rs
@@ -9,9 +9,8 @@ use common::{init_logger, run_polkadot_node, KillChildOnDrop};
 use pallet_election_provider_multi_phase::ReadySolution;
 use sp_storage::StorageChangeSet;
 use staking_miner::{
-	any_runtime,
 	opt::Chain,
-	prelude::{AccountId, Hash, SubxtClient},
+	prelude::{runtime, AccountId, Hash, SubxtClient},
 };
 use std::{
 	process,
@@ -42,38 +41,36 @@ async fn test_submit_solution(chain: Chain) {
 			.unwrap(),
 	);
 
-	any_runtime!(chain, {
-		let api = SubxtClient::from_url(&ws_url).await.unwrap();
+	let api = SubxtClient::from_url(&ws_url).await.unwrap();
 
-		let now = Instant::now();
+	let now = Instant::now();
 
-		let key = Bytes(
-			runtime::storage()
-				.election_provider_multi_phase()
-				.queued_solution()
-				.to_root_bytes(),
-		);
+	let key = Bytes(
+		runtime::storage()
+			.election_provider_multi_phase()
+			.queued_solution()
+			.to_root_bytes(),
+	);
 
-		let mut sub = api
-			.rpc()
-			.subscribe("state_subscribeStorage", rpc_params![vec![key]], "state_unsubscribeStorage")
-			.await
-			.unwrap();
+	let mut sub = api
+		.rpc()
+		.subscribe("state_subscribeStorage", rpc_params![vec![key]], "state_unsubscribeStorage")
+		.await
+		.unwrap();
 
-		let mut success = false;
+	let mut success = false;
 
-		while now.elapsed() < MAX_DURATION_FOR_SUBMIT_SOLUTION {
-			let x: StorageChangeSet<Hash> = sub.next().await.unwrap().unwrap();
+	while now.elapsed() < MAX_DURATION_FOR_SUBMIT_SOLUTION {
+		let x: StorageChangeSet<Hash> = sub.next().await.unwrap().unwrap();
 
-			if let Some(data) = x.changes[0].clone().1 {
-				let solution: ReadySolution<AccountId> = Decode::decode(&mut data.0.as_slice())
-					.expect("Failed to decode storage as QueuedSolution");
-				println!("solution: {:?}", solution);
-				success = true;
-				break
-			}
+		if let Some(data) = x.changes[0].clone().1 {
+			let solution: ReadySolution<AccountId> = Decode::decode(&mut data.0.as_slice())
+				.expect("Failed to decode storage as QueuedSolution");
+			println!("solution: {:?}", solution);
+			success = true;
+			break
 		}
+	}
 
-		assert!(success);
-	});
+	assert!(success);
 }
