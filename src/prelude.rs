5d5a19ae008e32aa11a43495dd43f0218906815b
James Wilson <james@jsdw.me>
2023-04-28 09:57:09 +0100

    Remove commented out error import
    Co-authored-by: Niklas Adolfsson <niklasadolfsson1@gmail.com>

--- src/prelude.rs
+++ src/prelude.rs
@@ -21,7 +21,6 @@
 //! needing to sprinkle `any_runtime` in a few extra places.
 
 // re-exports.
-// pub use crate::error::Error;
 pub use frame_election_provider_support::VoteWeight;
 pub use pallet_election_provider_multi_phase::{Miner, MinerConfig};
 pub use subxt::ext::sp_core;
