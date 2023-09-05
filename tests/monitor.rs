6883640d8ac4fc8c621aa5f3ef2707766d9fcf3b
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2023-09-05 15:10:00 +0200

    tests: disable integration tests `polkadot-dev` (#674)

--- tests/monitor.rs
+++ tests/monitor.rs
@@ -16,7 +16,8 @@ use std::{process, time::Instant};
 #[tokio::test]
 async fn submit_monitor_works_basic() {
 	init_logger();
-	test_submit_solution(Target::Node(Chain::Polkadot)).await;
+	// TODO: https://github.com/paritytech/staking-miner-v2/issues/673
+	// test_submit_solution(Target::Node(Chain::Polkadot)).await;
 	test_submit_solution(Target::Node(Chain::Kusama)).await;
 	test_submit_solution(Target::Node(Chain::Westend)).await;
 	test_submit_solution(Target::StakingMinerPlayground).await;
