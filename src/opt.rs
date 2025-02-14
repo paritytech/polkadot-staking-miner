cb77b622a8655c779cabfbb7fdf00d2905566d18
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2025-02-14 14:29:04 +0100

    cargo fmt

--- src/opt.rs
+++ src/opt.rs
@@ -27,110 +27,110 @@ use subxt::backend::legacy::rpc_methods as subxt_rpc;
 #[derive(Debug, Clone, Parser)]
 #[cfg_attr(test, derive(PartialEq))]
 pub enum Solver {
-	SeqPhragmen {
-		#[clap(long, default_value = "10")]
-		iterations: usize,
-	},
-	PhragMMS {
-		#[clap(long, default_value = "10")]
-		iterations: usize,
-	},
+    SeqPhragmen {
+        #[clap(long, default_value = "10")]
+        iterations: usize,
+    },
+    PhragMMS {
+        #[clap(long, default_value = "10")]
+        iterations: usize,
+    },
 }
 
 frame_support::parameter_types! {
-	/// Number of balancing iterations for a solution algorithm. Set based on the [`Solvers`] CLI
-	/// config.
-	pub static BalanceIterations: usize = 10;
-	pub static Balancing: Option<BalancingConfig> = Some( BalancingConfig { iterations: BalanceIterations::get(), tolerance: 0 } );
+    /// Number of balancing iterations for a solution algorithm. Set based on the [`Solvers`] CLI
+    /// config.
+    pub static BalanceIterations: usize = 10;
+    pub static Balancing: Option<BalancingConfig> = Some( BalancingConfig { iterations: BalanceIterations::get(), tolerance: 0 } );
 }
 
 /// The chain being used.
 #[derive(Debug, Copy, Clone)]
 pub enum Chain {
-	Westend,
-	Kusama,
-	Polkadot,
+    Westend,
+    Kusama,
+    Polkadot,
 }
 
 impl fmt::Display for Chain {
-	fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-		let chain = match self {
-			Self::Polkadot => "polkadot",
-			Self::Kusama => "kusama",
-			Self::Westend => "westend",
-		};
-		write!(f, "{}", chain)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let chain = match self {
+            Self::Polkadot => "polkadot",
+            Self::Kusama => "kusama",
+            Self::Westend => "westend",
+        };
+        write!(f, "{}", chain)
+    }
 }
 
 impl std::str::FromStr for Chain {
-	type Err = Error;
+    type Err = Error;
 
-	fn from_str(s: &str) -> Result<Self, Error> {
-		match s {
-			"polkadot" => Ok(Self::Polkadot),
-			"kusama" => Ok(Self::Kusama),
-			"westend" | "node" => Ok(Self::Westend),
-			chain => Err(Error::InvalidChain(chain.to_string())),
-		}
-	}
+    fn from_str(s: &str) -> Result<Self, Error> {
+        match s {
+            "polkadot" => Ok(Self::Polkadot),
+            "kusama" => Ok(Self::Kusama),
+            "westend" | "node" => Ok(Self::Westend),
+            chain => Err(Error::InvalidChain(chain.to_string())),
+        }
+    }
 }
 
 impl TryFrom<subxt_rpc::RuntimeVersion> for Chain {
-	type Error = Error;
+    type Error = Error;
 
-	fn try_from(rv: subxt_rpc::RuntimeVersion) -> Result<Self, Error> {
-		let json = rv
-			.other
-			.get("specName")
-			.expect("RuntimeVersion must have specName; qed")
-			.clone();
-		let mut chain =
-			serde_json::from_value::<String>(json).expect("specName must be String; qed");
-		chain.make_ascii_lowercase();
-		Chain::from_str(&chain)
-	}
+    fn try_from(rv: subxt_rpc::RuntimeVersion) -> Result<Self, Error> {
+        let json = rv
+            .other
+            .get("specName")
+            .expect("RuntimeVersion must have specName; qed")
+            .clone();
+        let mut chain =
+            serde_json::from_value::<String>(json).expect("specName must be String; qed");
+        chain.make_ascii_lowercase();
+        Chain::from_str(&chain)
+    }
 }
 
 // This is infallible because all these field must exist on substrate-based chains
 // and is based on <https://docs.rs/sp-version/latest/sp_version/struct.RuntimeVersion.html>
 impl From<subxt_rpc::RuntimeVersion> for RuntimeVersion {
-	fn from(rv: subxt_rpc::RuntimeVersion) -> Self {
-		let mut spec_name: String = get_val_unchecked("specName", &rv.other);
-		let impl_name: String = get_val_unchecked("implName", &rv.other);
-		let impl_version: u32 = get_val_unchecked("implVersion", &rv.other);
-		let authoring_version: u32 = get_val_unchecked("authoringVersion", &rv.other);
-		let state_version: u8 = get_val_unchecked("stateVersion", &rv.other);
+    fn from(rv: subxt_rpc::RuntimeVersion) -> Self {
+        let mut spec_name: String = get_val_unchecked("specName", &rv.other);
+        let impl_name: String = get_val_unchecked("implName", &rv.other);
+        let impl_version: u32 = get_val_unchecked("implVersion", &rv.other);
+        let authoring_version: u32 = get_val_unchecked("authoringVersion", &rv.other);
+        let state_version: u8 = get_val_unchecked("stateVersion", &rv.other);
 
-		let spec_version = rv.spec_version;
-		let transaction_version = rv.transaction_version;
+        let spec_version = rv.spec_version;
+        let transaction_version = rv.transaction_version;
 
-		spec_name.make_ascii_lowercase();
+        spec_name.make_ascii_lowercase();
 
-		Self {
-			spec_name,
-			impl_name,
-			impl_version,
-			spec_version,
-			transaction_version,
-			authoring_version,
-			state_version,
-		}
-	}
+        Self {
+            spec_name,
+            impl_name,
+            impl_version,
+            spec_version,
+            transaction_version,
+            authoring_version,
+            state_version,
+        }
+    }
 }
 
 #[derive(Deserialize, Serialize, PartialEq, Debug, Clone)]
 pub struct RuntimeVersion {
-	pub spec_name: String,
-	pub impl_name: String,
-	pub spec_version: u32,
-	pub impl_version: u32,
-	pub authoring_version: u32,
-	pub transaction_version: u32,
-	pub state_version: u8,
+    pub spec_name: String,
+    pub impl_name: String,
+    pub spec_version: u32,
+    pub impl_version: u32,
+    pub authoring_version: u32,
+    pub transaction_version: u32,
+    pub state_version: u8,
 }
 
 fn get_val_unchecked<T: DeserializeOwned>(val: &str, rv: &HashMap<String, serde_json::Value>) -> T {
-	let json = rv.get(val).expect("`{val}` must exist; qed").clone();
-	serde_json::from_value::<T>(json).expect("T must be Deserialize; qed")
+    let json = rv.get(val).expect("`{val}` must exist; qed").clone();
+    serde_json::from_value::<T>(json).expect("T must be Deserialize; qed")
 }
--- src/static_types.rs
+++ src/static_types.rs
@@ -16,71 +16,74 @@
 
 use crate::{epm, prelude::*};
 use polkadot_sdk::{
-	frame_election_provider_support::{self, traits::NposSolution},
-	frame_support::{self, traits::ConstU32, weights::Weight},
-	pallet_election_provider_multi_phase::{self, RawSolution, SolutionOrSnapshotSize},
-	sp_runtime,
+    frame_election_provider_support::{self, traits::NposSolution},
+    frame_support::{self, traits::ConstU32, weights::Weight},
+    pallet_election_provider_multi_phase::{self, RawSolution, SolutionOrSnapshotSize},
+    sp_runtime,
 };
 
 macro_rules! impl_atomic_u32_parameter_types {
-	($mod:ident, $name:ident) => {
-		mod $mod {
-			use std::sync::atomic::{AtomicU32, Ordering};
+    ($mod:ident, $name:ident) => {
+        mod $mod {
+            use std::sync::atomic::{AtomicU32, Ordering};
 
-			static VAL: AtomicU32 = AtomicU32::new(0);
+            static VAL: AtomicU32 = AtomicU32::new(0);
 
-			pub struct $name;
+            pub struct $name;
 
-			impl $name {
-				pub fn get() -> u32 {
-					VAL.load(Ordering::SeqCst)
-				}
-			}
+            impl $name {
+                pub fn get() -> u32 {
+                    VAL.load(Ordering::SeqCst)
+                }
+            }
 
-			impl<I: From<u32>> polkadot_sdk::frame_support::traits::Get<I> for $name {
-				fn get() -> I {
-					I::from(Self::get())
-				}
-			}
+            impl<I: From<u32>> polkadot_sdk::frame_support::traits::Get<I> for $name {
+                fn get() -> I {
+                    I::from(Self::get())
+                }
+            }
 
-			impl $name {
-				pub fn set(val: u32) {
-					VAL.store(val, std::sync::atomic::Ordering::SeqCst);
-				}
-			}
-		}
+            impl $name {
+                pub fn set(val: u32) {
+                    VAL.store(val, std::sync::atomic::Ordering::SeqCst);
+                }
+            }
+        }
 
-		pub use $mod::$name;
-	};
+        pub use $mod::$name;
+    };
 }
 
 mod max_weight {
-	use polkadot_sdk::frame_support::{self, weights::Weight};
-	use std::sync::atomic::{AtomicU64, Ordering};
+    use polkadot_sdk::frame_support::{self, weights::Weight};
+    use std::sync::atomic::{AtomicU64, Ordering};
 
-	static REF_TIME: AtomicU64 = AtomicU64::new(0);
-	static PROOF_SIZE: AtomicU64 = AtomicU64::new(0);
+    static REF_TIME: AtomicU64 = AtomicU64::new(0);
+    static PROOF_SIZE: AtomicU64 = AtomicU64::new(0);
 
-	pub struct MaxWeight;
+    pub struct MaxWeight;
 
-	impl MaxWeight {
-		pub fn get() -> Weight {
-			Weight::from_parts(REF_TIME.load(Ordering::SeqCst), PROOF_SIZE.load(Ordering::SeqCst))
-		}
-	}
+    impl MaxWeight {
+        pub fn get() -> Weight {
+            Weight::from_parts(
+                REF_TIME.load(Ordering::SeqCst),
+                PROOF_SIZE.load(Ordering::SeqCst),
+            )
+        }
+    }
 
-	impl frame_support::traits::Get<Weight> for MaxWeight {
-		fn get() -> Weight {
-			Self::get()
-		}
-	}
+    impl frame_support::traits::Get<Weight> for MaxWeight {
+        fn get() -> Weight {
+            Self::get()
+        }
+    }
 
-	impl MaxWeight {
-		pub fn set(weight: Weight) {
-			REF_TIME.store(weight.ref_time(), Ordering::SeqCst);
-			PROOF_SIZE.store(weight.proof_size(), Ordering::SeqCst);
-		}
-	}
+    impl MaxWeight {
+        pub fn set(weight: Weight) {
+            REF_TIME.store(weight.ref_time(), Ordering::SeqCst);
+            PROOF_SIZE.store(weight.proof_size(), Ordering::SeqCst);
+        }
+    }
 }
 
 impl_atomic_u32_parameter_types!(max_length, MaxLength);
@@ -89,175 +92,190 @@ impl_atomic_u32_parameter_types!(max_winners, MaxWinners);
 pub use max_weight::MaxWeight;
 
 pub mod westend {
-	use super::*;
+    use super::*;
 
-	// SYNC
-	frame_election_provider_support::generate_solution_type!(
-		#[compact]
-		pub struct NposSolution16::<
-			VoterIndex = u32,
-			TargetIndex = u16,
-			Accuracy = sp_runtime::PerU16,
-			MaxVoters = ConstU32::<22500>
-		>(16)
-	);
+    // SYNC
+    frame_election_provider_support::generate_solution_type!(
+        #[compact]
+        pub struct NposSolution16::<
+            VoterIndex = u32,
+            TargetIndex = u16,
+            Accuracy = sp_runtime::PerU16,
+            MaxVoters = ConstU32::<22500>
+        >(16)
+    );
 
-	#[derive(Debug)]
-	pub struct MinerConfig;
-	impl pallet_election_provider_multi_phase::unsigned::MinerConfig for MinerConfig {
-		type AccountId = AccountId;
-		type MaxLength = MaxLength;
-		type MaxWeight = MaxWeight;
-		type MaxVotesPerVoter = MaxVotesPerVoter;
-		type Solution = NposSolution16;
-		type MaxWinners = MaxWinners;
+    #[derive(Debug)]
+    pub struct MinerConfig;
+    impl pallet_election_provider_multi_phase::unsigned::MinerConfig for MinerConfig {
+        type AccountId = AccountId;
+        type MaxLength = MaxLength;
+        type MaxWeight = MaxWeight;
+        type MaxVotesPerVoter = MaxVotesPerVoter;
+        type Solution = NposSolution16;
+        type MaxWinners = MaxWinners;
 
-		fn solution_weight(
-			voters: u32,
-			targets: u32,
-			active_voters: u32,
-			desired_targets: u32,
-		) -> Weight {
-			let Some(votes) = epm::mock_votes(
-				active_voters,
-				desired_targets.try_into().expect("Desired targets < u16::MAX"),
-			) else {
-				return Weight::MAX;
-			};
+        fn solution_weight(
+            voters: u32,
+            targets: u32,
+            active_voters: u32,
+            desired_targets: u32,
+        ) -> Weight {
+            let Some(votes) = epm::mock_votes(
+                active_voters,
+                desired_targets
+                    .try_into()
+                    .expect("Desired targets < u16::MAX"),
+            ) else {
+                return Weight::MAX;
+            };
 
-			// Mock a RawSolution to get the correct weight without having to do the heavy work.
-			let raw = RawSolution {
-				solution: NposSolution16 { votes1: votes, ..Default::default() },
-				..Default::default()
-			};
+            // Mock a RawSolution to get the correct weight without having to do the heavy work.
+            let raw = RawSolution {
+                solution: NposSolution16 {
+                    votes1: votes,
+                    ..Default::default()
+                },
+                ..Default::default()
+            };
 
-			if raw.solution.voter_count() != active_voters as usize ||
-				raw.solution.unique_targets().len() != desired_targets as usize
-			{
-				return Weight::MAX;
-			}
+            if raw.solution.voter_count() != active_voters as usize
+                || raw.solution.unique_targets().len() != desired_targets as usize
+            {
+                return Weight::MAX;
+            }
 
-			futures::executor::block_on(epm::runtime_api_solution_weight(
-				raw,
-				SolutionOrSnapshotSize { voters, targets },
-			))
-			.expect("solution_weight should work")
-		}
-	}
+            futures::executor::block_on(epm::runtime_api_solution_weight(
+                raw,
+                SolutionOrSnapshotSize { voters, targets },
+            ))
+            .expect("solution_weight should work")
+        }
+    }
 }
 
 pub mod polkadot {
-	use super::*;
+    use super::*;
 
-	// SYNC
-	frame_election_provider_support::generate_solution_type!(
-		#[compact]
-		pub struct NposSolution16::<
-			VoterIndex = u32,
-			TargetIndex = u16,
-			Accuracy = sp_runtime::PerU16,
-			MaxVoters = ConstU32::<22500>
-		>(16)
-	);
+    // SYNC
+    frame_election_provider_support::generate_solution_type!(
+        #[compact]
+        pub struct NposSolution16::<
+            VoterIndex = u32,
+            TargetIndex = u16,
+            Accuracy = sp_runtime::PerU16,
+            MaxVoters = ConstU32::<22500>
+        >(16)
+    );
 
-	#[derive(Debug)]
-	pub struct MinerConfig;
-	impl pallet_election_provider_multi_phase::unsigned::MinerConfig for MinerConfig {
-		type AccountId = AccountId;
-		type MaxLength = MaxLength;
-		type MaxWeight = MaxWeight;
-		type MaxVotesPerVoter = MaxVotesPerVoter;
-		type Solution = NposSolution16;
-		type MaxWinners = MaxWinners;
+    #[derive(Debug)]
+    pub struct MinerConfig;
+    impl pallet_election_provider_multi_phase::unsigned::MinerConfig for MinerConfig {
+        type AccountId = AccountId;
+        type MaxLength = MaxLength;
+        type MaxWeight = MaxWeight;
+        type MaxVotesPerVoter = MaxVotesPerVoter;
+        type Solution = NposSolution16;
+        type MaxWinners = MaxWinners;
 
-		fn solution_weight(
-			voters: u32,
-			targets: u32,
-			active_voters: u32,
-			desired_targets: u32,
-		) -> Weight {
-			let Some(votes) = epm::mock_votes(
-				active_voters,
-				desired_targets.try_into().expect("Desired targets < u16::MAX"),
-			) else {
-				return Weight::MAX;
-			};
+        fn solution_weight(
+            voters: u32,
+            targets: u32,
+            active_voters: u32,
+            desired_targets: u32,
+        ) -> Weight {
+            let Some(votes) = epm::mock_votes(
+                active_voters,
+                desired_targets
+                    .try_into()
+                    .expect("Desired targets < u16::MAX"),
+            ) else {
+                return Weight::MAX;
+            };
 
-			// Mock a RawSolution to get the correct weight without having to do the heavy work.
-			let raw = RawSolution {
-				solution: NposSolution16 { votes1: votes, ..Default::default() },
-				..Default::default()
-			};
+            // Mock a RawSolution to get the correct weight without having to do the heavy work.
+            let raw = RawSolution {
+                solution: NposSolution16 {
+                    votes1: votes,
+                    ..Default::default()
+                },
+                ..Default::default()
+            };
 
-			if raw.solution.voter_count() != active_voters as usize ||
-				raw.solution.unique_targets().len() != desired_targets as usize
-			{
-				return Weight::MAX;
-			}
+            if raw.solution.voter_count() != active_voters as usize
+                || raw.solution.unique_targets().len() != desired_targets as usize
+            {
+                return Weight::MAX;
+            }
 
-			futures::executor::block_on(epm::runtime_api_solution_weight(
-				raw,
-				SolutionOrSnapshotSize { voters, targets },
-			))
-			.expect("solution_weight should work")
-		}
-	}
+            futures::executor::block_on(epm::runtime_api_solution_weight(
+                raw,
+                SolutionOrSnapshotSize { voters, targets },
+            ))
+            .expect("solution_weight should work")
+        }
+    }
 }
 
 pub mod kusama {
-	use super::*;
+    use super::*;
 
-	// SYNC
-	frame_election_provider_support::generate_solution_type!(
-		#[compact]
-		pub struct NposSolution24::<
-			VoterIndex = u32,
-			TargetIndex = u16,
-			Accuracy = sp_runtime::PerU16,
-			MaxVoters = ConstU32::<12500>
-		>(24)
-	);
+    // SYNC
+    frame_election_provider_support::generate_solution_type!(
+        #[compact]
+        pub struct NposSolution24::<
+            VoterIndex = u32,
+            TargetIndex = u16,
+            Accuracy = sp_runtime::PerU16,
+            MaxVoters = ConstU32::<12500>
+        >(24)
+    );
 
-	#[derive(Debug)]
-	pub struct MinerConfig;
-	impl pallet_election_provider_multi_phase::unsigned::MinerConfig for MinerConfig {
-		type AccountId = AccountId;
-		type MaxLength = MaxLength;
-		type MaxWeight = MaxWeight;
-		type MaxVotesPerVoter = MaxVotesPerVoter;
-		type Solution = NposSolution24;
-		type MaxWinners = MaxWinners;
+    #[derive(Debug)]
+    pub struct MinerConfig;
+    impl pallet_election_provider_multi_phase::unsigned::MinerConfig for MinerConfig {
+        type AccountId = AccountId;
+        type MaxLength = MaxLength;
+        type MaxWeight = MaxWeight;
+        type MaxVotesPerVoter = MaxVotesPerVoter;
+        type Solution = NposSolution24;
+        type MaxWinners = MaxWinners;
 
-		fn solution_weight(
-			voters: u32,
-			targets: u32,
-			active_voters: u32,
-			desired_targets: u32,
-		) -> Weight {
-			let Some(votes) = epm::mock_votes(
-				active_voters,
-				desired_targets.try_into().expect("Desired targets < u16::MAX"),
-			) else {
-				return Weight::MAX;
-			};
+        fn solution_weight(
+            voters: u32,
+            targets: u32,
+            active_voters: u32,
+            desired_targets: u32,
+        ) -> Weight {
+            let Some(votes) = epm::mock_votes(
+                active_voters,
+                desired_targets
+                    .try_into()
+                    .expect("Desired targets < u16::MAX"),
+            ) else {
+                return Weight::MAX;
+            };
 
-			// Mock a RawSolution to get the correct weight without having to do the heavy work.
-			let raw = RawSolution {
-				solution: NposSolution24 { votes1: votes, ..Default::default() },
-				..Default::default()
-			};
+            // Mock a RawSolution to get the correct weight without having to do the heavy work.
+            let raw = RawSolution {
+                solution: NposSolution24 {
+                    votes1: votes,
+                    ..Default::default()
+                },
+                ..Default::default()
+            };
 
-			if raw.solution.voter_count() != active_voters as usize ||
-				raw.solution.unique_targets().len() != desired_targets as usize
-			{
-				return Weight::MAX;
-			}
+            if raw.solution.voter_count() != active_voters as usize
+                || raw.solution.unique_targets().len() != desired_targets as usize
+            {
+                return Weight::MAX;
+            }
 
-			futures::executor::block_on(epm::runtime_api_solution_weight(
-				raw,
-				SolutionOrSnapshotSize { voters, targets },
-			))
-			.expect("solution_weight should work")
-		}
-	}
+            futures::executor::block_on(epm::runtime_api_solution_weight(
+                raw,
+                SolutionOrSnapshotSize { voters, targets },
+            ))
+            .expect("solution_weight should work")
+        }
+    }
 }
