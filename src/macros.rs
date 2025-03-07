e588f79fd177037aeea87d14f4abf4dcb8532f9a
Niklas Adolfsson <niklasadolfsson1@gmail.com>
2025-03-07 16:42:31 +0100

    fix mixed tabs/spaces

--- src/macros.rs
+++ src/macros.rs
@@ -6,99 +6,99 @@
 /// Example:
 /// impl_u32_parameter_type(module_name, Type);
 macro_rules! impl_u32_parameter_type {
-	($mod:ident, $name:ident) => {
-		mod $mod {
-			use std::sync::atomic::{AtomicU32, Ordering};
-			static VAL: AtomicU32 = AtomicU32::new(0);
-			pub struct $name;
+    ($mod:ident, $name:ident) => {
+        mod $mod {
+            use std::sync::atomic::{AtomicU32, Ordering};
+            static VAL: AtomicU32 = AtomicU32::new(0);
+            pub struct $name;
 
-			impl $name {
-				pub fn get() -> u32 {
-					VAL.load(Ordering::SeqCst)
-				}
-			}
-			impl<I: From<u32>> polkadot_sdk::frame_support::traits::Get<I> for $name {
-				fn get() -> I {
-					I::from(Self::get())
-				}
-			}
+            impl $name {
+                pub fn get() -> u32 {
+                    VAL.load(Ordering::SeqCst)
+                }
+            }
+            impl<I: From<u32>> polkadot_sdk::frame_support::traits::Get<I> for $name {
+                fn get() -> I {
+                    I::from(Self::get())
+                }
+            }
 
-			impl $name {
-				pub fn set(val: u32) {
-					VAL.store(val, std::sync::atomic::Ordering::SeqCst);
-				}
-			}
-		}
-		pub use $mod::$name;
-	};
+            impl $name {
+                pub fn set(val: u32) {
+                    VAL.store(val, std::sync::atomic::Ordering::SeqCst);
+                }
+            }
+        }
+        pub use $mod::$name;
+    };
 }
 
 // A helper to configure to correct MinerConfig depending on chain.
 #[allow(unused_macros)]
 macro_rules! for_legacy_runtime {
-	($chain:tt, $($code:tt)*) => {
-		match $chain {
-			$crate::opt::Chain::Polkadot => {
-				#[allow(unused)]
-				use $crate::static_types::legacy::polkadot::MinerConfig;
-				$($code)*
-			},
-			$crate::opt::Chain::Kusama => {
-				#[allow(unused)]
-				use $crate::static_types::legacy::kusama::MinerConfig;
-				$($code)*
-			},
-			$crate::opt::Chain::Westend => {
-				#[allow(unused)]
-				use $crate::static_types::legacy::westend::MinerConfig;
-				$($code)*
-			},
-			$crate::opt::Chain::AssetHubNext => {
-				#[allow(unused)]
-				use $crate::static_types::legacy::westend::MinerConfig;
-				$($code)*
-			}
-			$crate::opt::Chain::SubstrateNode => {
-				#[allow(unused)]
-				use $crate::static_types::legacy::node::MinerConfig;
-				$($code)*
-			}
-		}
-	};
+    ($chain:tt, $($code:tt)*) => {
+        match $chain {
+            $crate::opt::Chain::Polkadot => {
+                #[allow(unused)]
+                use $crate::static_types::legacy::polkadot::MinerConfig;
+                $($code)*
+            },
+            $crate::opt::Chain::Kusama => {
+                #[allow(unused)]
+                use $crate::static_types::legacy::kusama::MinerConfig;
+                $($code)*
+            },
+            $crate::opt::Chain::Westend => {
+                #[allow(unused)]
+                use $crate::static_types::legacy::westend::MinerConfig;
+                $($code)*
+            },
+            $crate::opt::Chain::AssetHubNext => {
+                #[allow(unused)]
+                use $crate::static_types::legacy::westend::MinerConfig;
+                $($code)*
+            }
+            $crate::opt::Chain::SubstrateNode => {
+                #[allow(unused)]
+                use $crate::static_types::legacy::node::MinerConfig;
+                $($code)*
+            }
+        }
+    };
 }
 
 // A helper to configure to correct MinerConfig depending on chain.
 #[allow(unused_macros)]
 macro_rules! for_multi_block_runtime {
-	($chain:tt, $($code:tt)*) => {
-		match $chain {
-			$crate::opt::Chain::Polkadot => {
-				#[allow(unused)]
-				use $crate::static_types::multi_block::polkadot::MinerConfig;
-				$($code)*
-			},
-			$crate::opt::Chain::Kusama => {
-				#[allow(unused)]
-				use $crate::static_types::multi_block::kusama::MinerConfig;
-				$($code)*
-			},
-			$crate::opt::Chain::Westend => {
-				#[allow(unused)]
-				use $crate::static_types::multi_block::westend::MinerConfig;
-				$($code)*
-			},
-			$crate::opt::Chain::AssetHubNext => {
-				#[allow(unused)]
-				use $crate::static_types::multi_block::westend::MinerConfig;
-				$($code)*
-			}
-			$crate::opt::Chain::SubstrateNode => {
-				#[allow(unused)]
-				use $crate::static_types::multi_block::node::MinerConfig;
-				$($code)*
-			}
-		}
-	};
+    ($chain:tt, $($code:tt)*) => {
+        match $chain {
+            $crate::opt::Chain::Polkadot => {
+                #[allow(unused)]
+                use $crate::static_types::multi_block::polkadot::MinerConfig;
+                $($code)*
+            },
+            $crate::opt::Chain::Kusama => {
+                #[allow(unused)]
+                use $crate::static_types::multi_block::kusama::MinerConfig;
+                $($code)*
+            },
+            $crate::opt::Chain::Westend => {
+                #[allow(unused)]
+                use $crate::static_types::multi_block::westend::MinerConfig;
+                $($code)*
+            },
+            $crate::opt::Chain::AssetHubNext => {
+                #[allow(unused)]
+                use $crate::static_types::multi_block::westend::MinerConfig;
+                $($code)*
+            }
+            $crate::opt::Chain::SubstrateNode => {
+                #[allow(unused)]
+                use $crate::static_types::multi_block::node::MinerConfig;
+                $($code)*
+            }
+        }
+    };
 }
 
 #[allow(unused)]
